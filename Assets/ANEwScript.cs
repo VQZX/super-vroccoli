50cfa8755d460ad5ac3c77c579a5c7b077335934
Flusk <tpflusk@gmail.com>
2023-04-19 16:49:56 +0200

    This is an attempt at a merge conflict

--- Assets/ANEwScript.cs
+++ Assets/ANEwScript.cs
@@ -7,7 +7,17 @@ public class ANEwScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     //This is a comment for demonstrative purposes   
+     
+	    
+	    
+	    
+	    
+	    
+	    //This is a comment for demonstrative purposes  
+     for (var i =0; i < 1000; i++)
+        {
+	        Debug.Log($"{i}");
+        }
     }
 
     // Update is called once per frame
