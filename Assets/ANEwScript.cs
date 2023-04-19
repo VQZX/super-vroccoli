c75bd485957f9ef569b0e7f66098a137f027455f
Keagan Bowler <53654571+1973457@users.noreply.github.com>
2023-04-19 16:37:41 +0200

    Initial Commit
    Added comment to ANEwScript

--- Assets/ANEwScript.cs
+++ Assets/ANEwScript.cs
@@ -7,7 +7,7 @@ public class ANEwScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     //This is a comment for demonstrative purposes   
     }
 
     // Update is called once per frame
